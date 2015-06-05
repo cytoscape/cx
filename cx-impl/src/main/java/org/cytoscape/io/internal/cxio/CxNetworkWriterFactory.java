26627ad0ab3218b9251c25b84d75df7df165d227
cmzmasek <cmzmasek@yahoo.com>
2015-06-05 15:33:41 -0700

    removed unneeded method

--- cx-impl/src/main/java/org/cytoscape/io/internal/cxio/CxNetworkWriterFactory.java
+++ cx-impl/src/main/java/org/cytoscape/io/internal/cxio/CxNetworkWriterFactory.java
@@ -6,18 +6,12 @@ import org.cytoscape.io.CyFileFilter;
 import org.cytoscape.io.write.CyWriter;
 import org.cytoscape.io.write.CyWriterFactory;
 import org.cytoscape.model.CyNetwork;
-import org.cytoscape.view.model.CyNetworkView;
 
 public class CxNetworkWriterFactory implements CyWriterFactory  {
 	private final CyFileFilter filter;
 	public CxNetworkWriterFactory(CyFileFilter filter) {
 		this.filter = filter;
 	}
-	
-	public CyWriter createWriter(OutputStream outputStream, CyNetworkView view) {
-		return new CxNetworkWriter(outputStream, view.getModel());
-	}
-
 	
 	public CyWriter createWriter(OutputStream outputStream, CyNetwork network) {
 		return new CxNetworkWriter(outputStream, network);
