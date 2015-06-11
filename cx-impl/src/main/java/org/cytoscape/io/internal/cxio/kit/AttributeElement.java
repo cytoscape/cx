564b4800a9c23581c82f93104b80e79219e670ca
cmzmasek <cmzmasek@yahoo.com>
2015-06-11 13:44:30 -0700

    in progress

---
+++ cx-impl/src/main/java/org/cytoscape/io/internal/cxio/kit/AttributeElement.java
@@ -0,0 +1,45 @@
+package org.cytoscape.io.internal.cxio.kit;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.SortedMap;
+
+import org.cytoscape.io.internal.cxio.kit.CxConstants.ATTRIBUTE_TYPE;
+
+public abstract class AttributeElement implements AspectElement {
+
+    SortedMap<String, List<String>> attributes;
+    String                          id;
+    CxConstants.ATTRIBUTE_TYPE      type;
+
+    public final void addAttribute(final String key, final String value) {
+        if (Util.isEmpty(key)) {
+            throw new IllegalArgumentException("attempt to use null or empty attribute key");
+        }
+        if (value == null) {
+            throw new IllegalArgumentException("attempt to use null value");
+        }
+        if (!attributes.containsKey(key)) {
+            attributes.put(key, new ArrayList<String>());
+        }
+        attributes.get(key).add(value);
+    }
+
+    public final String getId() {
+        return id;
+    }
+
+    public final ATTRIBUTE_TYPE getType() {
+        return type;
+    }
+
+    @Override
+    public int hashCode() {
+        return id.hashCode();
+    }
+
+    public final SortedMap<String, List<String>> getAttributes() {
+        return attributes;
+    }
+
+}
