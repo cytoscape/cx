28247b614e38930fa636e108f2f0b48560b9ad07
D <dotasek.dev@gmail.com>
2019-10-21 15:56:37 -0700

    Tests for edge and edgeAttributes removal from zero edge networks

--- src/test/java/org/cytoscape/io/cx/SpecialTest.java
+++ src/test/java/org/cytoscape/io/cx/SpecialTest.java
@@ -6,12 +6,23 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.util.Collection;
+import java.util.HashSet;
 import java.util.Map;
+import java.util.Set;
+import java.util.stream.Stream;
+
 import org.cytoscape.io.cx.helpers.TestUtil;
 import org.cytoscape.io.cx.helpers.TestUtil.CxReaderWrapper;
+import org.cytoscape.io.internal.AspectSet;
 import org.cytoscape.io.internal.CyServiceModule;
+import org.cytoscape.io.internal.cxio.CxUtil;
+import org.cytoscape.model.CyEdge;
 import org.cytoscape.model.CyNetwork;
+import org.cytoscape.model.CyNetworkFactory;
 import org.cytoscape.model.CyNode;
+import org.cytoscape.model.CyRow;
+import org.cytoscape.model.subnetwork.CyRootNetwork;
+import org.cytoscape.model.subnetwork.CySubNetwork;
 import org.cytoscape.view.model.ContinuousRange;
 import org.cytoscape.view.model.CyNetworkView;
 import org.cytoscape.view.model.CyNetworkViewFactory;
@@ -28,7 +39,10 @@ import org.ndexbio.cxio.aspects.datamodels.CartesianLayoutElement;
 import org.ndexbio.cxio.aspects.datamodels.NetworkAttributesElement;
 import org.ndexbio.cxio.aspects.datamodels.NetworkRelationsElement;
 import org.ndexbio.cxio.aspects.datamodels.SubNetworkElement;
+import org.ndexbio.cxio.core.CxElementReader2;
 import org.ndexbio.cxio.core.interfaces.AspectElement;
+import org.ndexbio.cxio.core.interfaces.AspectFragmentReader;
+import org.ndexbio.cxio.metadata.MetaDataCollection;
 import org.ndexbio.model.cx.NiceCXNetwork;
 
 public class SpecialTest {
@@ -36,6 +50,94 @@ public class SpecialTest {
 	@BeforeClass
 	public static void init() {
 		TestUtil.init();
+	}
+	
+	@Test
+	public void testEdgesAndEdgeAttributesForSingleEdgeNetwork() throws IOException {
+		
+			// If no CX IDs are set, use SUIDs
+			final CyNetworkFactory network_factory = CyServiceModule.getService(CyNetworkFactory.class);
+			final CyNetwork network = network_factory.createNetwork();
+			final CyNode node = network.addNode();
+			final CyNode nodeb = network.addNode();
+			
+			final CyEdge edge = network.addEdge(node, nodeb, true);
+				
+			System.out.println("network edges: " + network.getEdgeCount());
+			final ByteArrayOutputStream out = TestUtil.saveNetwork(network, false, false);
+			
+			final File outf = TestUtil.saveOutputStreamToFile(out, "zero_edges_test_output.cx");
+			// TODO: Delete test files on exit
+//			outf.deleteOnExit();
+			final FileInputStream export_in = new FileInputStream(outf);
+			
+			final Collection<String> aspects = AspectSet.getAspectNames();
+		        
+			final Set<AspectFragmentReader> all_readers = new HashSet<>();
+		        for (final AspectFragmentReader reader : AspectSet.getAspectFragmentReaders(aspects)) {
+		            all_readers.add(reader);
+		        }
+			
+			final CxElementReader2 r = new CxElementReader2(export_in, all_readers, true);
+	        final MetaDataCollection preMetaData = r.getPreMetaData();
+			final MetaDataCollection postMetaData = r.getPostMetaData();
+	      	
+			assertEquals( 1,  Stream.concat(
+	        	preMetaData != null ? preMetaData.getMetaData().stream() : Stream.empty(), 
+	        	postMetaData != null ? postMetaData.getMetaData().stream() : Stream.empty()).filter(
+	        		el -> el != null && "edges".equals(el.getName())
+	        	).count()
+	        );
+			assertEquals(1,  Stream.concat(
+		        	preMetaData != null ? preMetaData.getMetaData().stream() : Stream.empty(), 
+		        	postMetaData != null ? postMetaData.getMetaData().stream() : Stream.empty()).filter(
+		        		el -> el != null && "edgeAttributes".equals(el.getName())
+		        	).count()
+		    );
+	}
+	
+	@Test
+	public void testNoEdgesAndEdgeAttributesForZeroEdgeNetwork() throws IOException {
+		
+			// If no CX IDs are set, use SUIDs
+			final CyNetworkFactory network_factory = CyServiceModule.getService(CyNetworkFactory.class);
+			final CyNetwork network = network_factory.createNetwork();
+			final CyNode node = network.addNode();
+			final CyNode nodeb = network.addNode();
+			
+			//final CyEdge edge = network.addEdge(node, nodeb, true);
+				
+			System.out.println("network edges: " + network.getEdgeCount());
+			final ByteArrayOutputStream out = TestUtil.saveNetwork(network, false, false);
+			
+			final File outf = TestUtil.saveOutputStreamToFile(out, "zero_edges_test_output.cx");
+			// TODO: Delete test files on exit
+//			outf.deleteOnExit();
+			final FileInputStream export_in = new FileInputStream(outf);
+			
+			final Collection<String> aspects = AspectSet.getAspectNames();
+		        
+			final Set<AspectFragmentReader> all_readers = new HashSet<>();
+		        for (final AspectFragmentReader reader : AspectSet.getAspectFragmentReaders(aspects)) {
+		            all_readers.add(reader);
+		        }
+			
+			final CxElementReader2 r = new CxElementReader2(export_in, all_readers, true);
+	        final MetaDataCollection preMetaData = r.getPreMetaData();
+			final MetaDataCollection postMetaData = r.getPostMetaData();
+	      	
+			assertEquals(0,  Stream.concat(
+	        	preMetaData != null ? preMetaData.getMetaData().stream() : Stream.empty(), 
+	        	postMetaData != null ? postMetaData.getMetaData().stream() : Stream.empty()).filter(
+	        		el -> el != null && "edges".equals(el.getName())
+	        	).count()
+	        );
+			assertEquals(0,  Stream.concat(
+		        	preMetaData != null ? preMetaData.getMetaData().stream() : Stream.empty(), 
+		        	postMetaData != null ? postMetaData.getMetaData().stream() : Stream.empty()).filter(
+		        		el -> el != null && "edgeAttributes".equals(el.getName())
+		        	).count()
+		    );
 	}
 	
 	@Test
